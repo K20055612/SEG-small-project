2a927480fc03e3498bba6f9a45802ae17ea5ac71
Nicolae-Marian Gartu <nicolae-marian.gartu@kcl.ac.uk>
2021-11-16 13:58:09 +0000

    Create basic User model

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(),
+    chess_experience_level = models.IntegerField()
