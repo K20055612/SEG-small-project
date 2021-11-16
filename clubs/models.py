e076241aee5af1d6608e44c1b5fe952d77bd79f9
Nicolae-Marian Gartu <nicolae-marian.gartu@kcl.ac.uk>
2021-11-16 15:49:31 +0000

    Enforce input validation on User model

--- clubs/models.py
+++ clubs/models.py
@@ -1,6 +1,38 @@
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    bio = models.TextField(),
-    chess_experience_level = models.IntegerField(default=1)
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\w{3,}$',
+                message='Username must consist of at least three alphanumericals'
+                )
+            ]
+    )
+    first_name = models.CharField(
+        max_length=50,
+        blank=False
+    )
+    last_name = models.CharField(
+        max_length=50,
+        blank=False
+    )
+    email = models.EmailField(
+        unique=True,
+        blank=False
+    )
+    bio = models.CharField(
+        max_length=520,
+        blank=True
+    )
+    chess_experience_level = models.IntegerField(
+        default=0,
+        validators=[
+            MinValueValidator(limit_value=0),
+            MaxValueValidator(limit_value=5)
+            ]
+    )
--- clubs/tests.py
+++ clubs/tests.py
@@ -1,3 +1,170 @@
+"""Unit tests for the User model."""
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class UserModelTestCase(TestCase):
+    """Unit tests for the User model."""
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            'johndoe',
+            first_name='John',
+            last_name='Doe',
+            email='johndoe@example.org',
+            password='Password123',
+            bio='The quick brwon fox jumps over the lazy dog.',
+            chess_experience_level = 2
+        )
+
+    def test_valid_user(self):
+        self._assert_user_is_valid()
+
+
+    def test_username_cannot_be_blank(self):
+        self.user.username = ''
+        self._assert_user_is_invalid()
+
+    def test_username_can_be_30_characters_long(self):
+        self.user.username = 'x' * 30
+        self._assert_user_is_valid()
+
+    def test_username_cannot_be_over_30_characters_long(self):
+        self.user.username = 'x' * 31
+        self._assert_user_is_invalid()
+
+    def test_username_must_be_unique(self):
+        second_user = self.create_second_user()
+        self.user.username = second_user.username
+        self._assert_user_is_invalid()
+
+    def test_username_must_contain_only_alphanumericals(self):
+        self.user.username = 'john!doe'
+        self._assert_user_is_invalid()
+
+    def test_username_must_contain_at_least_3_alphanumericals(self):
+        self.user.username = 'jo'
+        self._assert_user_is_invalid()
+
+    def test_username_may_contain_numbers(self):
+        self.user.username = 'j0hndoe2'
+        self._assert_user_is_valid()
+
+
+    def test_first_name_must_not_be_blank(self):
+        self.user.first_name = ''
+        self._assert_user_is_invalid()
+
+    def test_first_name_need_not_be_unique(self):
+        second_user = self.create_second_user()
+        self.user.first_name = second_user.first_name
+        self._assert_user_is_valid()
+
+    def test_first_name_may_contain_50_characters(self):
+        self.user.first_name = 'x' * 50
+        self._assert_user_is_valid()
+
+    def test_first_name_must_not_contain_more_than_50_characters(self):
+        self.user.first_name = 'x' * 51
+        self._assert_user_is_invalid()
+
+
+    def test_last_name_must_not_be_blank(self):
+        self.user.last_name = ''
+        self._assert_user_is_invalid()
+
+    def test_last_name_need_not_be_unique(self):
+        second_user = self.create_second_user()
+        self.user.last_name = second_user.last_name
+        self._assert_user_is_valid()
+
+    def test_last_name_may_contain_50_characters(self):
+        self.user.last_name = 'x' * 50
+        self._assert_user_is_valid()
+
+    def test_last_name_must_not_contain_more_than_50_characters(self):
+        self.user.last_name = 'x' * 51
+        self._assert_user_is_invalid()
+
+
+    def test_email_must_not_be_blank(self):
+        self.user.email = ''
+        self._assert_user_is_invalid()
+
+    def test_email_must_be_unique(self):
+        second_user = self.create_second_user()
+        self.user.email = second_user.email
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_username(self):
+        self.user.email = '@example.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_at_symbol(self):
+        self.user.email = 'johndoe.example.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_domain_name(self):
+        self.user.email = 'johndoe@.org'
+        self._assert_user_is_invalid()
+
+    def test_email_must_contain_domain(self):
+        self.user.email = 'johndoe@example'
+        self._assert_user_is_invalid()
+
+    def test_email_must_not_contain_more_than_one_at(self):
+        self.user.email = 'johndoe@@example.org'
+        self._assert_user_is_invalid()
+
+
+    def test_bio_may_be_blank(self):
+        self.user.bio = ''
+        self._assert_user_is_valid()
+
+    def test_bio_need_not_be_unique(self):
+        second_user = self.create_second_user()
+        self.user.bio = second_user.bio
+        self._assert_user_is_valid()
+
+    def test_bio_may_contain_520_characters(self):
+        self.user.bio = 'x' * 520
+        self._assert_user_is_valid()
+
+    def test_bio_must_not_contain_more_than_520_characters(self):
+        self.user.bio = 'x' * 521
+        self._assert_user_is_invalid()
+
+    def test_chess_experience_level_must_not_be_less_than_0(self):
+        self.user.chess_experience_level = -1
+        self._assert_user_is_invalid()
+
+    def test_chess_experience_level_must_not_be_greater_than_5(self):
+        self.user.chess_experience_level = 6
+        self._assert_user_is_invalid()
+
+    def test_chess_experience_level_may_be_greater_or_equal_to_0_and_less_or_equal_to_5(self):
+        self.user.chess_experience_level = 3
+        self._assert_user_is_valid()
+
+
+    def create_second_user(self):
+        user = User.objects.create_user(
+            'janedoe',
+            first_name='Jane',
+            last_name='Doe',
+            email='janedoe@example.org',
+            password='Password123',
+            bio='The quick brwon fox jumps over the lazy dog.',
+            chess_experience_level = 2
+        )
+        return user
+
+    def _assert_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except (ValidationError):
+            self.fail('Test user should be valid')
+
+    def _assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
