985239ef0fc1c2f4a31699dec945ad2ea5b79b3d
josefsousa09 <jfrancisco.mail@gmail.com>
2021-11-20 23:47:46 +0000

    Improve urls.py.

--- system/urls.py
+++ system/urls.py
@@ -1,5 +1,4 @@
 """system URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -20,5 +19,4 @@ from clubs import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('sign_up/', views.sign_up, name='sign_up'),
 ]
