f20033bdc5f9b62efaa1b1f45074247388946d44
Zirui Ding <zirui.ding@kcl.ac.uk>
2021-12-13 19:08:40 +0000

    delete login prohibited in helpers/py

--- clubs/helpers.py
+++ clubs/helpers.py
@@ -3,15 +3,6 @@ from django.conf import settings
 from .models import User,Club,Role
 from django.core.exceptions import ObjectDoesNotExist
 
-"""check whether the user is login"""
-def login_prohibited(view_function):
-    def modified_view_function(request):
-        if request.user.is_authenticated:
-            return redirect(settings.REDIRECT_URL_WHEN_LOGGED_IN)
-        else:
-            return view_function(request)
-    return modified_view_function
-
 """check whether the user is an officer or an owner"""
 def management_required(view_function):
     def modified_view_function(request,club_name,*args,**kwargs):
