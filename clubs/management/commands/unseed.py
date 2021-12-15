0e57671711c81f962167a3dacc8d3bd3c93deea1
Nicolae-Marian Gartu <nicolae-marian.gartu@kcl.ac.uk>
2021-12-15 19:16:21 +0200

    Refactor unseed as models already use ON DELETE CASCADE

--- clubs/management/commands/unseed.py
+++ clubs/management/commands/unseed.py
@@ -4,9 +4,4 @@ from clubs.models import User,Club,Role
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User.objects.filter(username__contains='@fake.seed').delete()
-        User.objects.filter(username__contains='@example.org').delete()
         Club.objects.filter(location__contains='Test').delete()
-        Role.objects.filter(club_role__contains='APP').delete()
-        Role.objects.filter(club_role__contains='OFF').delete()
-        Role.objects.filter(club_role__contains='MEM').delete()
-        Role.objects.filter(club_role__contains='OWN').delete()
